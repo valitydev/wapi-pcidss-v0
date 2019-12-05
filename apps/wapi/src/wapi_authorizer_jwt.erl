6678487f5d3bc1796a76b23ea5aa51ee2f77a7af
Roman Pushkov <pushkovroman@me.com>
2019-12-05 21:23:31 +0300

    FF-147: remove token expiration in wallets (#16)
    * remove token expiration handling
    
    * remove expired error type
    
    * change deletion to commenting out

--- apps/wapi/src/wapi_authorizer_jwt.erl
+++ apps/wapi/src/wapi_authorizer_jwt.erl
@@ -224,7 +224,7 @@ sign(#{kid := KID, jwk := JWK, signer := #{} = JWS}, Claims) ->
             badarg |
             {badarg, term()} |
             {missing, atom()} |
-            expired |
+            % expired |
             {malformed_acl, term()}
         } |
         {nonexistent_key, kid()} |
@@ -307,8 +307,11 @@ get_alg(#{}) ->
 get_validators() ->
     [
         {token_id   , <<"jti">> , fun check_presence/2},
-        {subject_id , <<"sub">> , fun check_presence/2},
-        {expires_at , <<"exp">> , fun check_expiration/2}
+        {subject_id , <<"sub">> , fun check_presence/2}
+        % TODO: temporarily disabled
+        %       might make a comeback if we will be able
+        %       to issue long-term tokens
+        % {expires_at , <<"exp">> , fun check_expiration/2}
     ].
 
 check_presence(_, V) when is_binary(V) ->
@@ -316,19 +319,19 @@ check_presence(_, V) when is_binary(V) ->
 check_presence(C, undefined) ->
     throw({invalid_token, {missing, C}}).
 
-check_expiration(_, Exp = 0) ->
-    Exp;
-check_expiration(_, Exp) when is_integer(Exp) ->
-    case genlib_time:unow() of
-        Now when Exp > Now ->
-            Exp;
-        _ ->
-            throw({invalid_token, expired})
-    end;
-check_expiration(C, undefined) ->
-    throw({invalid_token, {missing, C}});
-check_expiration(C, V) ->
-    throw({invalid_token, {badarg, {C, V}}}).
+% check_expiration(_, Exp = 0) ->
+%     Exp;
+% check_expiration(_, Exp) when is_integer(Exp) ->
+%     case genlib_time:unow() of
+%         Now when Exp > Now ->
+%             Exp;
+%         _ ->
+%             throw({invalid_token, expired})
+%     end;
+% check_expiration(C, undefined) ->
+%     throw({invalid_token, {missing, C}});
+% check_expiration(C, V) ->
+%     throw({invalid_token, {badarg, {C, V}}}).
 
 %%
 
