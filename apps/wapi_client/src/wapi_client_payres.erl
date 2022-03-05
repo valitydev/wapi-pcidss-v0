fba4b3f79c5ce95cce7be6aa4fde8b31c02b441e
kehitt <kehitt@users.noreply.github.com>
2022-03-05 16:49:50 +0300

    fix dialyzer

--- apps/wapi_client/src/wapi_client_payres.erl
+++ apps/wapi_client/src/wapi_client_payres.erl
@@ -13,7 +13,7 @@ store_bank_card(Context, Request) ->
     Response = swag_client_payres_payment_resources_api:store_bank_card(Url, PreparedParams, Opts),
     wapi_client_lib:handle_response(Response).
 
--spec get_bank_card(context(), map()) -> {ok, #{binary() => _}} | {error, term()}.
+-spec get_bank_card(context(), binary()) -> {ok, #{binary() => _}} | {error, term()}.
 get_bank_card(Context, Token) ->
     Params = #{binding => #{<<"token">> => Token}},
     {Url, PreparedParams, Opts} = wapi_client_lib:make_request(Context, Params),
