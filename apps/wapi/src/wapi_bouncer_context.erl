1040f5f5f7f602dc2bb3618538bd70d7febfaa9c
anatoliy.losev <losto@nix>
2023-09-27 11:53:57 +0300

    TD-720: bump bouncer proto

--- apps/wapi/src/wapi_bouncer_context.erl
+++ apps/wapi/src/wapi_bouncer_context.erl
@@ -1,6 +1,6 @@
 -module(wapi_bouncer_context).
 
--include_lib("bouncer_proto/include/bouncer_context_v1_thrift.hrl").
+-include_lib("bouncer_proto/include/bouncer_ctx_v1_thrift.hrl").
 
 -type fragment() :: bouncer_client:context_fragment().
 -type acc() :: bouncer_context_helpers:context_fragment().
@@ -43,9 +43,9 @@ build(Prototypes, {Acc0, External}) ->
     {Acc1, External}.
 
 build(operation, #{id := OperationID}, Acc) ->
-    Acc#bctx_v1_ContextFragment{
-        wapi = #bctx_v1_ContextWalletAPI{
-            op = #bctx_v1_WalletAPIOperation{
+    Acc#ctx_v1_ContextFragment{
+        wapi = #ctx_v1_ContextWalletAPI{
+            op = #ctx_v1_WalletAPIOperation{
                 id = operation_id_to_binary(OperationID)
             }
         }
